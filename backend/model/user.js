0b75e0e20e5fce5a5022badec8c90d735509920d
abhi2322 <abhishekjamwal34@gmail.com>
2021-11-24 23:47:24 +0530

    editing userSchema

--- backend/model/user.js
+++ backend/model/user.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema({
   last_name: { type: String, default: null },
   email: { type: String, unique: true },
   password: { type: String },
-  token: { type: String },
 });
 
 module.exports = mongoose.model("user", userSchema);
